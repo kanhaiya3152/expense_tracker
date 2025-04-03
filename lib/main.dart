105dfb74ed9758a3dbf1bac1cb9ffde48118ce96
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-04-03 14:32:12 +0530

    Implement authentication flow with Firebase, redirecting to LoginScreen or ExpenseScreen based on user state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:expense_tracker/expense_screen.dart';
+import 'package:expense_tracker/screen/expense_screen.dart';
+import 'package:expense_tracker/screen/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -23,7 +25,18 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  ExpenseScreen(),
+      // home:  LoginScreen(),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              
+            }
+            if (snapshot.hasData) {
+              return ExpenseScreen();
+            }
+            return LoginScreen();
+          }),
     );
   }
 }
